d0f471058319fa45f65627b46e09bd38b162bfd5
GennertL <75732481+GennertL@users.noreply.github.com>
2020-12-17 20:34:16 +0200

    Add files via upload

---
+++ Ülesanne 1.py
@@ -0,0 +1,15 @@
+# Gennert Lensment
+# 17.11.2020
+# Ülesanne 1
+
+#escape string \ - välistab järgmise märgi süntaksina
+print("\"Cabernet Sauvignon\" või 'Cabernet Sauvignon'?"
+print(" @..@")
+print(" (----)")
+print("( \__/ )")
+print('^^ "" ^^ ')
+#kasutaja sisestus
+a = int(input("Sisesta ruudu külg: ")))
+ruuduS = a * a
+print(ruuduS)
+print("Ruudu pindala on:",ruuduS)
